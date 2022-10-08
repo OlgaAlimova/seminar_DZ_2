e9ceb1394c56c40e91f808ebe880bea0c09eda7e
olga alimova <alim-olgaru@yamdex.ru>
2022-10-09 01:38:49 +0300

    DZ

---
+++ Track43/Program.cs
@@ -0,0 +1,49 @@
+﻿// Напишите программу, которая будет создавать
+// копию заданного массива с помощью поэлементного
+// копирования.
+
+Console.WriteLine("Введите размер массива: ");
+int sizeTask = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите минимальное число диапазона массива: ");
+int minTask = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите максимальное число диапазона массива: ");
+int maxTask = Convert.ToInt32(Console.ReadLine());
+
+maxTask = maxTask + 1;
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    var rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+void PrintElementArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+}
+
+int[] arr = CreateArrayRndInt(sizeTask, minTask, maxTask);
+PrintArray(arr);
+PrintElementArray(arr);
